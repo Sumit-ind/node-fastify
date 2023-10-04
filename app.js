9d420ec13e21d8958012abeebc9c8c02cbc50e08
Sumit Yadav <sumit.yadav1@indegene.com>
2023-10-04 14:43:47 +0530

    Refactored the application file, added routes, db connection.

--- app.js
+++ app.js
@@ -1,31 +1,19 @@
 const fastify = require('fastify')({ logger: true });
+const mongoose = require('mongoose')
+
+// Import routes
+const userRoute = require('./routes/user.route')
 
 const PORT = process.env.PORT || 5500;
 
-fastify.get('/', (req, reply) => {
-  const query = req.query;
-  console.log('query :>> ', query);
-  reply.code(200).send("Hello world!")
-})
+// Register Routes
+fastify.register(userRoute, { prefix: '/api/v1/users' })
 
-fastify.post("/", (req, reply) => {
-  const body = req.body;
-  console.log('body :>> ', body);
-  reply.code(400).send([{ msg: "This is post" }])
-})
-
-fastify.put('/:id/:category', (req, reply) => {
-  const params = req.params;
-
-  console.log('params :>> ', params);
-  reply.send({ msg: "This is put" })
-})
-
-fastify.delete('/', (req, reply) => {
-  const query = req.query;
-  console.log('query :>> ', query);
-  reply.send({ msg: "This is delete" })
-})
+// Connect DB
+mongoose.connect('YOUR_MONGODB_STRING', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(_ => console.log('Connected DB...')).catch(err => console.log('err :>> ', err));
 
 const app = async () => {
   await fastify.listen({ port: PORT })
